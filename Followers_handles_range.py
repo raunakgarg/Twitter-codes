66e4c7709811e19cc4b23669470fddd214fe181b
Raunak garg <raunak.garg@microfocus.com>
2019-11-16 03:37:52 +0530

    Commit 15th nov

--- Followers_handles_range.py
+++ Followers_handles_range.py
@@ -22,7 +22,7 @@ auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True,
 				   wait_on_rate_limit_notify=True)
 api.followers_ids
-users = tweepy.Cursor(api.followers, screen_name="raunakgarg2").items()
+users = tweepy.Cursor(api.followers, screen_name="PramodkumarTriv").items()
 temp = []
 while True:
     try:
@@ -39,24 +39,25 @@ dataset["handle_name"] = new[0]
 dataset["followers"] = new[1]
 dataset[["followers"]] = dataset[["followers"]].apply(pd.to_numeric)
 dataset = dataset.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+dataset.to_csv("C:\\Users\\gargrau.corpdom\\Desktop\\python_by_ganeshan\\Twitter codes\\PramodkumarTriv\\twitter.csv")
 dataset2 = dataset[dataset['followers'].between(10000, 20000)] 
 dataset2.rename(columns={'followers':'followers between 10000 and 20000'}, inplace=True)
-os.makedirs("C:\\Users\\gargrau.corpdom\\Desktop\\python_by_ganeshan\\Twitter codes\\raunakgarg2", exist_ok=True) 
-dataset2.to_csv("C:\\Users\\gargrau.corpdom\\Desktop\\python_by_ganeshan\\Twitter codes\\raunakgarg2\\twitter1.csv")
+os.makedirs("C:\\Users\\gargrau.corpdom\\Desktop\\python_by_ganeshan\\Twitter codes\\PramodkumarTriv", exist_ok=True) 
+dataset2.to_csv("C:\\Users\\gargrau.corpdom\\Desktop\\python_by_ganeshan\\Twitter codes\\PramodkumarTriv\\twitter1.csv")
 dataset3 = dataset[dataset['followers'].between(5000, 10000)] 
 dataset3.rename(columns={'followers':'followers between 5000 and 10000'}, inplace=True)
-dataset3.to_csv("C:\\Users\\gargrau.corpdom\\Desktop\\python_by_ganeshan\\Twitter codes\\raunakgarg2\\twitter2.csv")
+dataset3.to_csv("C:\\Users\\gargrau.corpdom\\Desktop\\python_by_ganeshan\\Twitter codes\\PramodkumarTriv\\twitter2.csv")
 dataset4 = dataset[dataset['followers'].between(2000, 5000)] 
 dataset4.rename(columns={'followers':'followers between 2000 and 5000'}, inplace=True)
-dataset4.to_csv("C:\\Users\\gargrau.corpdom\\Desktop\\python_by_ganeshan\\Twitter codes\\raunakgarg2\\twitter3.csv")
+dataset4.to_csv("C:\\Users\\gargrau.corpdom\\Desktop\\python_by_ganeshan\\Twitter codes\\PramodkumarTriv\\twitter3.csv")
 dataset5 = dataset[dataset['followers'].between(1000, 2000)] 
 dataset5.rename(columns={'followers':'followers between 1000 and 2000'}, inplace=True)
-dataset5.to_csv("C:\\Users\\gargrau.corpdom\\Desktop\\python_by_ganeshan\\Twitter codes\\raunakgarg2\\twitter4.csv")
+dataset5.to_csv("C:\\Users\\gargrau.corpdom\\Desktop\\python_by_ganeshan\\Twitter codes\\PramodkumarTriv\\twitter4.csv")
 dataset6 = dataset[dataset['followers'].between(400, 1000)] 
 dataset6.rename(columns={'followers':'followers between 400 and 1000'}, inplace=True)
-dataset6.to_csv("C:\\Users\\gargrau.corpdom\\Desktop\\python_by_ganeshan\\Twitter codes\\raunakgarg2\\twitter5.csv")
+dataset6.to_csv("C:\\Users\\gargrau.corpdom\\Desktop\\python_by_ganeshan\\Twitter codes\\PramodkumarTriv\\twitter5.csv")
 dataset7 = dataset[dataset['followers'].between(200, 400)] 
 dataset7.rename(columns={'followers':'followers between 200 and 400'}, inplace=True)
-dataset7.to_csv("C:\\Users\\gargrau.corpdom\\Desktop\\python_by_ganeshan\\Twitter codes\\raunakgarg2\\twitter6.csv")
+dataset7.to_csv("C:\\Users\\gargrau.corpdom\\Desktop\\python_by_ganeshan\\Twitter codes\\PramodkumarTriv\\twitter6.csv")
 
 #dataset.dtypes
